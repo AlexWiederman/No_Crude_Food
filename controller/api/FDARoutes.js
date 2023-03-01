de19f76defe6edafa30bd937d487bd08adb07e3a
Naldo47 <reinaldo.santana444@gmail.com>
2023-02-28 19:14:22 -0500

    savedRecalls created

--- controller/api/FDARoutes.js
+++ controller/api/FDARoutes.js
@@ -1,19 +1,18 @@
-const express = require('express');
-const router = express.Router();
-const fetch = require('node-fetch');
+const express = require('express')
+const router = express.Router()
+const fetch = require('node-fetch')
 
 // Route to get FDA information
 router.get('/fdaInfo', async (req, res) => {
-
   try {
-    const response = await fetch('https://api.fda.gov/drug/label.json');
-    const data = await response.json();
-    
-    res.status(200).json(data);
+    const response = await fetch('https://api.fda.gov/drug/label.json')
+    const data = await response.json()
+
+    res.status(200).json(data)
   } catch (err) {
     // Handle any errors that occur during fetch request
-    res.status(500).json({ message: 'Error fetching FDA information' });
+    res.status(500).json({ message: 'Error fetching FDA information' })
   }
-});
+})
 
-module.exports = router;
+module.exports = router
