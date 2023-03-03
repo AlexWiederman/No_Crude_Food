1a66ae06a6d3ffe7c27f4f0d4a58c91f359a234d
AvivaRubenstein <avivadrubenstein@gmail.com>
2023-03-02 19:43:09 -0500

    hid api key in .env

--- controller/api/FDARoutes.js
+++ controller/api/FDARoutes.js
@@ -3,6 +3,7 @@ const express = require('express')
 const router = express.Router()
 const { UserManufacturer } = require('../../model')
 const withAuth = require('../../utilities/auth')
+require('dotenv').config()
 
 let reqUrl
 /* const resultsArray = [] */
@@ -20,13 +21,12 @@ router.get('/', withAuth, async (req, res) => {
 
     // replace spaces in string with "+" symbol
 
-    
     // loop through all of the manufacturers of a user to get all of the api requests per manufacturer
     for (let i = 0; i < datas.length; i++) {
       const apiManufac = datas[i].manufacturer_name.split(' ').join('+')
       // console.warn(apiManufac)
 
-      reqUrl = `https://api.fda.gov/food/enforcement.json?api_key=5khaeeOSSl7L3hc7vWcvW6IOKIkMgqRpss9Vfz4X&search=recalling_firm:"${apiManufac}"+AND+status.exact:Ongoing&limit=5`
+      reqUrl = `https://api.fda.gov/food/enforcement.json?api_key=${process.env.API_KEY}&search=recalling_firm:"${apiManufac}"+AND+status.exact:Ongoing&limit=5`
       // urlArray = urlArray.push(reqUrl)
       /* urls[i] = { url: reqUrl } */
     }
