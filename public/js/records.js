37e1e3555502ce6d054d1fb30ed71811ea7e07ad
Naldo47 <reinaldo.santana444@gmail.com>
2023-03-04 16:25:04 -0500

    save button linked, needs work on data

--- public/js/records.js
+++ public/js/records.js
@@ -1,42 +1,42 @@
 
 //  button to get data from /api/FDARoutes
-const getDataButton = document.createElement('button')
-getDataButton.innerText = 'Get Data'
-getDataButton.addEventListener('click', async () => {
-  const id = 1 
-  const response = await fetch(`/api/FDARoutes=${id}`)
-  if (response.ok) {
-    const data = await response.json()
-    console.log(data) 
-  } else {
-    console.error('Failed to fetch data')
-  }
-})
-document.body.appendChild(getDataButton)
+// const getDataButton = document.createElement('button')
+// getDataButton.innerText = 'Get Data'
+// getDataButton.addEventListener('click', async () => {
+//   const id = 1
+//   const response = await fetch(`/api/FDARoutes=${id}`)
+//   if (response.ok) {
+//     const data = await response.json()
+//     console.log(data)
+//   } else {
+//     console.error('Failed to fetch data')
+//   }
+// })
+// document.body.appendChild(getDataButton)
 
-//  button to save data to /api/FDARoutes 
-const saveDataButton = document.createElement('button')
-saveDataButton.innerText = 'Save Data'
-saveDataButton.addEventListener('click', async () => {
-  const id = 1 
+//  button to save data to /api/FDARoutes
+function saveData (id) {
   const data = {}
-   
-  
-  const response = await fetch(`/api/FDARoutes=${id}`, {
+
+  fetch(`/api/FDARoutes=${id}`, {
     method: 'POST',
     body: JSON.stringify(data),
     headers: { 'Content-Type': 'application/json' }
+  }).then(response => {
+    if (response.ok) {
+      console.log('Data saved successfully')
+    } else {
+      console.error('Failed to save data')
+    }
   })
-  if (response.ok) {
-    console.log('Data saved successfully')
-  } else {
-    console.error('Failed to save data')
-  }
+}
+
+// Add event listener to each button
+const saveDataButtons = document.querySelectorAll('.save-data-button')
+saveDataButtons.forEach(button => {
+  const id = button.dataset.id
+
+  button.addEventListener('click', () => {
+    saveData(id)
+  })
 })
-document.body.appendChild(saveDataButton)
-
-
-
-
-
-<script src="./js/login.js"></script>
